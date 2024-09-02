264a7bb0e683cfb52d9f4018bcc7049a7198d5eb
Mihir Lochan Maruvada <mihir.maruvada@gmail.com>
2024-09-02 16:52:15 -0500

    Initial Commit for Hello.java program(Prints Hello World Message)

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+    /**
+     * Mihir Lochan Maruvada
+     * 2024/08/29
+     * Prints my name
+     */
+    public static void main(String[] args) {
+        System.out.println("Mihir Lochan Maruvada");
+    }
+}
